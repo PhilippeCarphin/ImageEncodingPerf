e325c05935f15711a95dedb91761c49367870cda
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-01-27 13:56:45 -0500

    Transformé byte-pair code to function and added decode function

--- src/algo/bytepair.py
+++ src/algo/bytepair.py
@@ -1,54 +1,92 @@
 
 import numpy as np
 import regex as re
-Message = "ABAABAABACABBABCDAADACABABAAABAABBABABAABAAB"
-LUToctetsdispo = [True] * 256
-dictsymb =[Message[0]]
-LUToctetsdispo[ord(Message[0])] = False
-nbsymboles = 1
-for i in range(1,len(Message)):
-    if Message[i] not in dictsymb:
-        dictsymb += [Message[i]]
-        LUToctetsdispo[ord(Message[i])] = False  #Octet utilisé
-        nbsymboles += 1
 
-longueurOriginale = np.ceil(np.log2(nbsymboles))*len(Message)
+def byte_pair_encode(message):
+    """
+    Encode a message using byte-pair encoding
+
+    message (str) : The message to encode
+
+    returns a dictionary containing the encoded message and a list of the substitutions that were made
+
+        {
+            "coded_message": message,
+            "dict_symb": dict_symb
+        }
+    """
+
+    LUToctetsdispo = [True] * 256
+    dict_symb =[message[0]]
+    LUToctetsdispo[ord(message[0])] = False
+    nbsymboles = 1
+    for i in range(1,len(message)):
+        if message[i] not in dict_symb:
+            dict_symb += [message[i]]
+            LUToctetsdispo[ord(message[i])] = False  #Octet utilisé
+            nbsymboles += 1
+
+    longueurOriginale = np.ceil(np.log2(nbsymboles))*len(message)
 
 
-dictsymb = []  #Dictionnaire des substitutions
-debut = ord(Message[0])  # Origine trouver un code de substitution. Et pour avoir des caractères imprimables...
+    dict_symb = []  #Dictionnaire des substitutions
+    debut = ord(message[0])  # Origine trouver un code de substitution. Et pour avoir des caractères imprimables...
 
-remplacementpossible = True
-while remplacementpossible == True:
-    #Recherche des paires
-    paires = []
-    for i in range(0,len(Message)-1):
-        temppaire = Message[i]+Message[i+1]
-        if not list(filter(lambda x: x[0] == temppaire, paires)): #Si la liste retournée par filter est vide.
-            paires += [[temppaire,len(re.findall(temppaire, Message, overlapped = True))]]
+    remplacementpossible = True
+    while remplacementpossible == True:
+        #Recherche des paires
+        paires = []
+        for i in range(0,len(message)-1):
+            temppaire = message[i]+message[i+1]
+            if not list(filter(lambda x: x[0] == temppaire, paires)): #Si la liste retournée par filter est vide.
+                paires += [[temppaire,len(re.findall(temppaire, message, overlapped = True))]]
 
-    #Trouve la paire avec le plus de répétitions.
-    paires = sorted(paires, key=lambda x: x[1], reverse = True)
+        #Trouve la paire avec le plus de répétitions.
+        paires = sorted(paires, key=lambda x: x[1], reverse = True)
 
-    if paires[0][1] > 1:
-        #Remplace la paire
-        print(paires)
-        print("La paire ",paires[0][0], " est la plus fréquente avec ",paires[0][1], "répétitions")
-        #Cherche un octet non utilisé
-        while debut <256 and LUToctetsdispo[debut] == False:
-            debut += 1
-        if debut < 256:
-            #On substitut
-            Message = Message.replace(paires[0][0], chr(debut))
-            LUToctetsdispo[debut] = False
-            dictsymb += [[paires[0][0], chr(debut)]]
+        if paires[0][1] > 1:
+            #Remplace la paire
+            # print(paires)
+            # print("La paire ",paires[0][0], " est la plus fréquente avec ",paires[0][1], "répétitions")
+            #Cherche un octet non utilisé
+            while debut <256 and LUToctetsdispo[debut] == False:
+                debut += 1
+            if debut < 256:
+                #On substitut
+                message = message.replace(paires[0][0], chr(debut))
+                LUToctetsdispo[debut] = False
+                dict_symb += [[paires[0][0], chr(debut)]]
+            else:
+                print("Il n'y a plus d'octets disponible!") #Bien sûr, ce n'est pas exact car la recherche commence à message[0]
+
+            # print(message)
+            # print(dict_symb)
         else:
-            print("Il n'y a plus d'octets disponible!") #Bien sûr, ce n'est pas exact car la recherche commence à Message[0]
+            remplacementpossible = False
 
-        print(Message)
-        print(dictsymb)
+    # print("Longueur = {0}".format(np.ceil(np.log2(nbsymboles))*len(message)))
+    # print("Longueur originale = {0}".format(longueurOriginale))
+    # print(dictsymb)
+    return {"coded_message": message, "dict_symb": dict_symb}
+
+def byte_pair_decode(coded_message, dict_symb):
+    """ Decode a message encoded by byte-pair algorithm """
+    for subst in reversed(dict_symb):
+        coded_message = coded_message.replace(subst[1], subst[0])
+
+    return coded_message
+
+def test_byte_pair():
+    Message = "ABAABAABACABBABCDAADACABABAAABAABBABABAABAAB"
+    result = byte_pair_encode(Message)
+    decoded_message = byte_pair_decode(result["coded_message"], result["dict_symb"])
+
+    if decoded_message == Message:
+        print("TEST PASSED : decoded message is the same as initial message")
     else:
-        remplacementpossible = False
+        print("TEST FAILED : decoded message does not match initial message: \n    in : {}\n   out : {}"
+              .format(Message, decoded_message))
 
-print("Longueur = {0}".format(np.ceil(np.log2(nbsymboles))*len(Message)))
-print("Longueur originale = {0}".format(longueurOriginale))
+
+if __name__ == "__main__":
+    test_byte_pair()
