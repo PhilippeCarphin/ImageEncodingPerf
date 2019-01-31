eed4e03ff6c9a439c12d54c7dcbd899a31e0a33e
Adam Martin-Côté <adammartincote@gmail.com>
2019-01-31 18:08:29 -0500

    compute compresion

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
+import os
 
 
 def get_size_in_bytes(file) -> int:
-    return 1
+    size = os.path.getsize(file)
+    return size
 
 
 def get_compression_factor(**kwargs) -> float:
-    return 0.42
+    try:
+        after = kwargs['after']
+        before = kwargs['before']
+    except KeyError:
+        raise FileNotFoundError
+
+    factor = 1 - (get_size_in_bytes(after)\
+                 /get_size_in_bytes(before))
+    return factor
